ffece781ddb38859aa00a289310ac6844cf80b7b
vortex9 <vortex.jedi@gmail.com>
2014-03-18 16:13:07 +0300

    Доработана версия для печати. https://github.com/vortex9/E3/issues/4

--- sites/all/themes/entas/templates/node.tpl.php
+++ sites/all/themes/entas/templates/node.tpl.php
@@ -89,9 +89,7 @@
   <?php print $user_picture; ?>
 
   <?php print render($title_prefix); ?>
-  <?php if (!$page && $title): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-  <?php endif; ?>
+  
   <?php print render($title_suffix); ?>
 
   <?php if ($unpublished): ?>
