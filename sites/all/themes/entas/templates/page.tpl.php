24e539043c2c7bd665ab88917de7c4cc452d2168
vortex9 <vortex.jedi@gmail.com>
2014-05-12 18:21:29 +0300

    https://github.com/vortex9/E3/issues/89

--- sites/all/themes/entas/templates/page.tpl.php
+++ sites/all/themes/entas/templates/page.tpl.php
@@ -83,10 +83,10 @@
 	<link rel="apple-touch-icon" sizes="72x72" href="/sites/all/themes/entas/images/apple-touch-icon/apple-touch-icon-72x72.png" />
 	<link rel="apple-touch-icon" sizes="114x114" href="/sites/all/themes/entas/images/apple-touch-icon/apple-touch-icon-114x114.png" />
 	<link rel="apple-touch-icon" sizes="144x144" href="/sites/all/themes/entas/images/apple-touch-icon/apple-touch-icon-144x144.png" />
-	<link rel="apple-touch-icon" sizes="57x57" href="/sites/all/themes/entas/images/apple-touch-icon/apple-touch-icon-60x60.png" />
-	<link rel="apple-touch-icon" sizes="72x72" href="/sites/all/themes/entas/images/apple-touch-icon/apple-touch-icon-120x120.png" />
-	<link rel="apple-touch-icon" sizes="114x114" href="/sites/all/themes/entas/images/apple-touch-icon/apple-touch-icon-76x76.png" />
-	<link rel="apple-touch-icon" sizes="144x144" href="/sites/all/themes/entas/images/apple-touch-icon/apple-touch-icon-152x152.png" />
+	<link rel="apple-touch-icon" sizes="60x60" href="/sites/all/themes/entas/images/apple-touch-icon/apple-touch-icon-60x60.png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="/sites/all/themes/entas/images/apple-touch-icon/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="/sites/all/themes/entas/images/apple-touch-icon/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="/sites/all/themes/entas/images/apple-touch-icon/apple-touch-icon-152x152.png" />
 
 <!-- RSS потоки -->
 <link rel="alternate" type="application/rss+xml" title="Мероприятия ЭНТАС" href="/entas_event.xml" />
