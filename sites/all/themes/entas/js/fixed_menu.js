79bbd6cb15b85a0f9b8cd681596c5ec40d1726f1
vortex9 <vortex.jedi@gmail.com>
2014-06-13 16:46:43 +0300

    Правильный скрипт для фиксированного меню

--- sites/all/themes/entas/js/fixed_menu.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function() {
-				if ($(this).scrollTop() > 50) {
-                    $('#navigation').addClass('fixed');
-		        } else {
-		            $('#navigation').removeClass('fixed');
-		        }
-		});
---
+++ sites/all/themes/entas/js/fixed_menu3.js
@@ -0,0 +1,11 @@
+var num = 50; //number of pixels before modifying styles
+
+jQuery(window).bind('scroll', function () {
+    if (jQuery(window).scrollTop() > num) {
+        jQuery('.scroll-menu').addClass('fixed');
+		jQuery('.scroll-logo').addClass('fixed-logo')
+    } else {
+        jQuery('.scroll-menu').removeClass('fixed');
+		jQuery('.scroll-logo').removeClass('fixed-logo');
+    }
+});
