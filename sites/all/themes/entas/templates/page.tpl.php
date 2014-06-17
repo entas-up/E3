f9338628019acdaddb4691fe863a735c9c52f874
vortex9 <vortex.jedi@gmail.com>
2014-06-17 18:38:21 +0300

    В шаблоне убран лишний класс

--- sites/all/themes/entas/templates/page.tpl.php
+++ sites/all/themes/entas/templates/page.tpl.php
@@ -99,7 +99,7 @@
   <div id="header"><div class="section clearfix">
     <?php if ($logo): ?>
         
-<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" class="scroll-logo" alt="<?php print t('Home'); ?>" /></a>
+<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
 <?php if ($site_name || $site_slogan): ?>
       <div id="name-and-slogan">
         <?php if ($site_name): ?>
