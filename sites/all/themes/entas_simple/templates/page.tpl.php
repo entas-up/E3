0b891fa6d7598ebc63fe4d6cbe0dc8cb05450e92
vortex9 <vortex.jedi@gmail.com>
2016-05-24 12:19:14 +0300

    Убрана лишние файлы

--- sites/all/themes/entas_simple/templates/page.tpl.php
+++ sites/all/themes/entas_simple/templates/page.tpl.php
@@ -72,70 +72,23 @@
 
 <div id="page-wrapper"><div id="page">
 
-  <div id="header"><div class="section clearfix">
-
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-    <?php endif; ?>
-
-    <?php if ($site_name || $site_slogan): ?>
-      <div id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <?php if ($title): ?>
-            <div id="site-name"><strong>
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </strong></div>
-          <?php else: /* Use h1 when the content title is empty */ ?>
-            <h1 id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
-        <?php endif; ?>
-
-        <?php if ($site_slogan): ?>
-          <div id="site-slogan"><?php print $site_slogan; ?></div>
-        <?php endif; ?>
-      </div><!-- /#name-and-slogan -->
-    <?php endif; ?>
-    
-    <?php print render($page['header']); ?>
-
-  </div></div><!-- /.section, /#header -->
-
-  <div id="main-wrapper"><div id="main" class="clearfix">
-
-    <div id="content" class="column"><div class="section">
-      
-      <?php print $breadcrumb; ?>
-      <a id="main-content"></a>
+  <div id="header">
+      <a href="http://entas.ru" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="http://entas.ru/sites/all/themes/entas/logo.png" alt="<?php print t('Home'); ?>" /></a>
+       <div id="name-and-slogan"><div id="site-slogan"><h1><?php print $site_slogan; ?></h1></div></div><!-- /#name-and-slogan -->
+        
+  </div>
+   <div id="content" class="column"><div class="section">
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
         <h1 class="title" id="page-title"><?php print $title; ?></h1>
       <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if ($tabs = render($tabs)): ?>
-        <div class="tabs"><?php print $tabs; ?></div>
-      <?php endif; ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
       <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
-    </div></div><!-- /.section, /#content -->
-
-    
-
-      </div></div><!-- /.section, /#navigation -->
-   
-
-    
-
-  </div></div><!-- /#main, /#main-wrapper -->
-
+      
+    </div></div>
+      
+  </div></div>
+<hr>
   <?php print render($page['footer']); ?>
 
-</div></div><!-- /#page, /#page-wrapper -->
 
 <?php print render($page['bottom']); ?>
