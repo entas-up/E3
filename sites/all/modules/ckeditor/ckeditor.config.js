adc4dd29b44f1c8806c37f0f0cbd22b010856173
Vitaliy (github work) <vortex.jedi@gmail.com>
2021-09-24 16:31:43 +0300

    updated module ckeditor

--- sites/all/modules/ckeditor/ckeditor.config.js
+++ sites/all/modules/ckeditor/ckeditor.config.js
@@ -33,6 +33,9 @@ config.protectedSource.push(/<i[\s\S]*?>[\s\S]*?<\/i>/g); //allows  <i>
   // config.protectedSource.push(/<code>[\s\S]*?<\/code>/gi);
   config.extraPlugins = '';
 
+  // Insert all Smiley image paths as relative or they may fail on SSL pages.
+  config.smiley_path = window.CKEDITOR_BASEPATH + 'plugins/smiley/images/';
+
   /*
     * Append here extra CSS rules that should be applied into the editing area.
     * Example:
@@ -63,6 +66,11 @@ config.protectedSource.push(/<i[\s\S]*?>[\s\S]*?<\/i>/g); //allows  <i>
   if (Drupal.settings.ckeditor.theme == "marinelli") {
     config.bodyClass = 'singlepage';
     config.bodyId = 'primary';
+  }
+
+  // Make CKEditor's edit area as high as the textarea would be.
+  if (this.element.$.rows > 0) {
+    config.height = this.element.$.rows * 20 + 'px';
   }
 }
 
@@ -105,3 +113,21 @@ Drupal.settings.cke_toolbar_DrupalFull = [
   ['Maximize', 'ShowBlocks'],
   ['DrupalBreak', 'DrupalPageBreak']
 ];
+
+/**
+* To enable browser native spell checker uncomment (default) first of following two lines.
+* The second line (is a default setting and can leave commented) allows access to browser context menu with ctrl-right-click
+* otherwise there is no access to the browser context menu without further config changes
+*/
+CKEDITOR.config.disableNativeSpellChecker = false;
+// CKEDITOR.config.browserContextMenuOnCtrl = true;
+
+/** NOT TESTED
+* For browser default context menu on right-click rather than ctrl-right-click
+* Note: Disabling CKEditor's context menu may render it impossible to work with tables
+* uncomment following lines;
+* three plugins need to be removed because scayt depends on menubutton which depends on contextmenu
+*/
+// config.browserContextMenuOnCtrl = false;
+// config.removePlugins = 'scayt,menubutton,contextmenu';
+
